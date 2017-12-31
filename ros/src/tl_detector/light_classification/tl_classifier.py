3b45a766e6634605b946bc732fe24649e91866cd
Andreas Ntalakas <antalakas@gmail.com>
2017-12-31 20:06:40 +0200

    tf session initializes only once

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -34,6 +34,9 @@ class TLClassifier(object):
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
 
+        self.sess=tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.5)),
+                             graph=self.detection_graph)
+
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
         Args:
@@ -46,68 +49,67 @@ class TLClassifier(object):
         traffic_light_color = TrafficLight.UNKNOWN
 
         with self.detection_graph.as_default():
-            with tf.Session(graph=self.detection_graph) as sess:
-                # Definite input and output Tensors for detection_graph
-                image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            # Definite input and output Tensors for detection_graph
+            image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
 
-                # Each box represents a part of the image where a particular object was detected.
-                detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
 
-                # Each score represent how level of confidence for each of the objects.
-                # Score is shown on the result image, together with the class label.
-                detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-                detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-                num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
 
-                # ------------------------------------- Image identification -------------------------------------------
-                # the array based representation of the image will be used later in order to prepare the
-                # result image with boxes and labels on it.
+            # ------------------------------------- Image identification -------------------------------------------
+            # the array based representation of the image will be used later in order to prepare the
+            # result image with boxes and labels on it.
 
-                image_np = np.asarray(image)
-                rospy.loginfo(image_np.shape)
-                # return TrafficLight.UNKNOWN
+            image_np = np.asarray(image)
+            rospy.loginfo(image_np.shape)
+            # return TrafficLight.UNKNOWN
 
-                # image_np = self.load_image_into_numpy_array(image)
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image_np_expanded = np.expand_dims(image_np, axis=0)
+            # image_np = self.load_image_into_numpy_array(image)
+            # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
 
-                time0 = time.time()
+            time0 = time.time()
 
-                # Actual detection.
-                (boxes, scores, classes, num) = sess.run(
-                    [detection_boxes, detection_scores, detection_classes, num_detections],
-                    feed_dict={image_tensor: image_np_expanded})
+            # Actual detection.
+            (boxes, scores, classes, num) = self.sess.run(
+                [detection_boxes, detection_scores, detection_classes, num_detections],
+                feed_dict={image_tensor: image_np_expanded})
 
-                time1 = time.time()
+            time1 = time.time()
 
-                rospy.loginfo('------------------------------')
-                rospy.loginfo("Time in milliseconds: " + str((time1 - time0) * 1000))
+            rospy.loginfo('------------------------------')
+            rospy.loginfo("Time in milliseconds: " + str((time1 - time0) * 1000))
 
-                boxes = np.squeeze(boxes)
-                scores = np.squeeze(scores)
-                classes = np.squeeze(classes).astype(np.int32)
+            boxes = np.squeeze(boxes)
+            scores = np.squeeze(scores)
+            classes = np.squeeze(classes).astype(np.int32)
 
-                min_score_thresh = .50
-                for i in range(boxes.shape[0]):
-                    if scores is None or scores[i] > min_score_thresh:
-                        class_name = self.category_index[classes[i]]['name']
+            min_score_thresh = .50
+            for i in range(boxes.shape[0]):
+                if scores is None or scores[i] > min_score_thresh:
+                    class_name = self.category_index[classes[i]]['name']
 
-                        if class_name == 'Red':
-                            if scores[i] >= 0.5:
-                                traffic_light_color = TrafficLight.RED
+                    if class_name == 'Red':
+                        if scores[i] >= 0.5:
+                            traffic_light_color = TrafficLight.RED
 
-                        if class_name == 'Green':
-                            if scores[i] >= 0.5:
-                                traffic_light_color = TrafficLight.GREEN
+                    if class_name == 'Green':
+                        if scores[i] >= 0.5:
+                            traffic_light_color = TrafficLight.GREEN
 
-                        if class_name == 'Yellow':
-                            if scores[i] >= 0.5:
-                                traffic_light_color = TrafficLight.YELLOW
+                    if class_name == 'Yellow':
+                        if scores[i] >= 0.5:
+                            traffic_light_color = TrafficLight.YELLOW
 
-                        if class_name == 'off':
-                                traffic_light_color = TrafficLight.UNKNOWN
+                    if class_name == 'off':
+                            traffic_light_color = TrafficLight.UNKNOWN
 
-                        # rospy.loginfo('class name: ' + class_name + ", score: " + str(scores[i]))
+                    # rospy.loginfo('class name: ' + class_name + ", score: " + str(scores[i]))
 
         return traffic_light_color
 
